26f97e06d629357be2199a027c0a5fdd7a7e9e13
田原 一樹 <ssp0727.lnc@gmail.com>
2021-10-16 12:37:55 +0900

    update

--- src/Render.test.jsx
+++ src/Render.test.jsx
@@ -5,6 +5,16 @@ import Render from './Render'
 describe('Rendering', () => {
   it('Should render all the elements correctly', () => {
     render(<Render />)
-    screen.debug()
+    // https://github.com/A11yance/aria-query#elements-to-roles
+    // screen.debug(screen.getByRole('heading'))
+    // https://jestjs.io/docs/en/expect
+    expect(screen.getByRole('heading')).toBeTruthy()
+    expect(screen.getByRole('textbox')).toBeTruthy()
+    expect(screen.getAllByRole('button')[0]).toBeTruthy()
+    expect(screen.getAllByRole('button')[1]).toBeTruthy()
+    expect(screen.getByText('Udemy')).toBeTruthy()
+    expect(screen.queryByText('Uddddd')).toBeNull()
+    expect(screen.getByTestId('copyright')).toBeTruthy()
+    // screen.debug(screen.getByText('Udemy'))
   })
 })
